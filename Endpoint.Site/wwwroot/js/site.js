a5cb310ea669af9bbbd67820689a01de9eb6afcb
ashkan5fdp <ashkan5fdp@gmail.com>
2021-11-16 20:47:16 +0330

    just test home page

--- Endpoint.Site/wwwroot/js/site.js
+++ Endpoint.Site/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//slider js 
+var slide = document.getElementsByClassName('slide');
+var prev = document.querySelector('.prev');
+var next = document.querySelector('.next');
+var n = 0;
 
-// Write your JavaScript code.
+function disnon() {
+    for (var i = 0; i < slide.length; i++) {
+        slide[i].style.display = 'none';
+    }
+}
+
+next.addEventListener("click", function (e) {
+    e.preventDefault();
+    n++;
+    if (n > slide.length - 1) {
+        n = 0;
+    }
+    disnon();
+    slide[n].style.display = 'block';
+
+});
+prev.addEventListener("click", function (e) {
+    e.preventDefault();
+    n--;
+    if (n < 0) {
+        n = slide.length - 1;
+    }
+    disnon();
+    slide[n].style.display = 'block';
+
+});
+
+setInterval(function () {
+    n++;
+    if (n > slide.length - 1) {
+        n = 0;
+    }
+    disnon();
+    slide[n].style.display = 'block';
+}, 3000)
